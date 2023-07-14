18ae19c07074cf0d69babe89b1c122a9a35a2a6d
CHERUKURU INDRASENA REDDY <indrasena0018@gmail.com>
2023-07-14 15:14:55 +0530

    addind default clinic name

--- src/screens/patientsearch.js
+++ src/screens/patientsearch.js
@@ -1,5 +1,5 @@
-import {Text, View, StyleSheet} from 'react-native';
-import { useState } from 'react';
+import {Text, View, StyleSheet, Pressable} from 'react-native';
+import {useState, useRef, useEffect} from 'react';
 import {
   CUSTOMCOLOR,
   CUSTOMFONTFAMILY,
@@ -14,45 +14,85 @@ import AppointmentCard from '../components/appointmentcard';
 import PlusButton from '../components/plusbtn';
 import PatientSearchCard from '../components/patientsearchcard';
 import {ScrollView} from 'react-native-gesture-handler';
-import { Icon, InputText } from '../components';
+import {Icon, InputText} from '../components';
+import {CONSTANTS} from '../utility/constant';
+import {BottomSheetView} from '../components';
+
 const PatientSearch = ({navigation}) => {
-  const [name,setName]=useState('')
-    const ChangeNameValue=(e)=>{
-      setName(e);
-    }
+  const [name, setName] = useState('');
+  const ChangeNameValue = e => {
+    setName(e);
+  };
+  const ClinicRef = useRef(null);
+
+  const clinics = CONSTANTS.clinic;
+
+  const [selectedClinic, setSelectedClinic] = useState(clinics[0]);
+  const handleClinicSelection = clinic => {
+    setSelectedClinic(clinic);
+    ClinicRef?.current?.snapToIndex(0);
+  };
   return (
     <View style={styles.main}>
       <ScrollView>
-      <View style={styles.select}>
-        <SelectorBtn name="chevron-down" />
-        <SelectorBtn name="calendar" />
-        {/* <SearchBox label='Patient name/phone number' action={()=>console.log('clicked')}/> */}
-      </View>
-      <View style={{flexDirection:'row',justifyContent:'space-around',alignItems:'center',bottom:16}}>
-            <InputText 
-            placeholder='Search name'
+        <View style={styles.select}>
+          <SelectorBtn
+            name="chevron-down"
+            input={selectedClinic}
+            onPress={() => ClinicRef?.current?.snapToIndex(1)}
+          />
+          {/* <SearchBox label='Patient name/phone number' action={()=>console.log('clicked')}/> */}
+        </View>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-around',
+            alignItems: 'center',
+            bottom: 16,
+          }}>
+          <InputText
+            placeholder="Search name"
             value={name}
             setValue={ChangeNameValue}
             textStyle={styles.input}
             keypad="numeric"
-            />
-            <Icon name='search' size={20} style={styles.searchIcon}/>
-            </View>
+          />
+          <Icon name="search" size={20} style={styles.searchIcon} />
+        </View>
 
-      <View style={styles.appointment}>
-        <Text style={styles.h2}>My Patients</Text>
+        <View style={styles.appointment}>
+          <Text style={styles.h2}>My Patients</Text>
 
-        <PatientSearchCard onPress={() => navigation.navigate('visit')} />
-        <PatientSearchCard onPress={() => navigation.navigate('visit')} />
-        <PatientSearchCard onPress={() => navigation.navigate('visit')} />
-        <PatientSearchCard onPress={() => navigation.navigate('visit')} />
-        <PatientSearchCard onPress={() => navigation.navigate('visit')} />
-        <PatientSearchCard onPress={() => navigation.navigate('visit')} />
-        <PatientSearchCard onPress={() => navigation.navigate('visit')} />
-      </View>
+          <PatientSearchCard onPress={() => navigation.navigate('visit')} />
+          <PatientSearchCard onPress={() => navigation.navigate('visit')} />
+          <PatientSearchCard onPress={() => navigation.navigate('visit')} />
+          <PatientSearchCard onPress={() => navigation.navigate('visit')} />
+          <PatientSearchCard onPress={() => navigation.navigate('visit')} />
+          <PatientSearchCard onPress={() => navigation.navigate('visit')} />
+          <PatientSearchCard onPress={() => navigation.navigate('visit')} />
+        </View>
       </ScrollView>
 
       {/* <PlusButton icon='plus'style={{position:'absolute',right:24,bottom:24}}/> */}
+      <BottomSheetView bottomSheetRef={ClinicRef} snapPoints={'50%'}>
+        <View style={styles.modalContainer}>
+          <Text
+            style={{
+              fontFamily: CUSTOMFONTFAMILY.heading,
+              fontSize: 18,
+              color: CUSTOMCOLOR.black,
+            }}>
+            {Language[language]['clinic']}
+          </Text>
+          {clinics?.map((clinic, index) => (
+            <Pressable
+              key={index}
+              onPress={() => handleClinicSelection(clinic)}>
+              <Text style={styles.modalfields}>{clinic}</Text>
+            </Pressable>
+          ))}
+        </View>
+      </BottomSheetView>
     </View>
   );
 };
@@ -62,7 +102,6 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 24,
     gap: 4,
- 
   },
   select: {
     gap: 8,
@@ -81,19 +120,34 @@ const styles = StyleSheet.create({
     lineHeight: 20 * 2,
     color: CUSTOMCOLOR.black,
   },
-  input:{
-    width:556,
-    height:51,
-    padding:16,
-    gap:4
-
-},
-searchIcon:{
-  top:10,
-    height:51,
-    right:10,
-    padding:16,
-},
+  input: {
+    width: '105%',
+    padding: 16,
+    gap: 4,
+  },
+  searchIcon: {
+    top: 10,
+    height: 51,
+    right: 10,
+    padding: 16,
+  },
+  modalContainer: {
+    height: 400,
+    width: '100%',
+    //justifyContent: 'center',
+    alignItems: 'flex-start',
+    backgroundColor: CUSTOMCOLOR.white,
+    alignSelf: 'center',
+    borderRadius: 10,
+    padding: 16,
+  },
+  modalfields: {
+    color: CUSTOMCOLOR.primary,
+    fontSize: 14,
+    fontWeight: 400,
+    fontFamily: CUSTOMFONTFAMILY.body,
+    padding: 4,
+  },
 });
 
 export default PatientSearch;
