076cf82bc683633052b359ce784bcd085d569dff
Indra211 <indrasena0018@gmail.com>
2024-01-09 12:49:26 +0530

    id added

--- src/redux/features/profiles/clinicAddress.js
+++ src/redux/features/profiles/clinicAddress.js
@@ -4,17 +4,22 @@ const ClinicAddress = createSlice({
   name: 'address',
   initialState: {
     address: '',
+    appointment_id: '',
   },
   reducers: {
     addAddress: (state, action) => {
       state.address = action.payload;
     },
     updateAddress: (state, action) => {
-        state.address = action.payload;
-      },
+      state.address = action.payload;
+    },
+    addAppointmentID: (state, action) => {
+      state.appointment_id = action.payload;
+    },
   },
 });
 
-export const {addAddress,updateAddress} = ClinicAddress.actions;
+export const {addAddress, updateAddress, addAppointmentID} =
+  ClinicAddress.actions;
 
 export const addressReducer = ClinicAddress.reducer;
